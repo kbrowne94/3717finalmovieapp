d12d31bb3dcdbed5b31fbed2ce69dd4fc56a4c79
Kyle Browne <kbrowne5@my.bcit.ca>
2020-04-16 09:59:22 -0700

    Initial commit

---
+++ app/src/main/java/ca/kbrowne3717/movieapp/MainActivity.java
@@ -0,0 +1,14 @@
+package ca.kbrowne3717.movieapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
